09c8a0c1536941b80fb0fa2a2690ad0fa2beb99e
Tushar <tusharvashisth4@gmail.com>
2025-11-01 14:43:47 +0530

    added one route and did minor changes in workflow file

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,23 @@ app.get("/", (req: Request, res: Response) => {
   return res.send("It's working 🙌");
 });
 
+
+// Array of quotes
+const quotes: string[] = [
+  "The only way to do great work is to love what you do. – Steve Jobs",
+  "Believe you can and you're halfway there. – Theodore Roosevelt",
+  "The future belongs to those who believe in the beauty of their dreams. – Eleanor Roosevelt",
+  "You miss 100% of the shots you don't take. – Wayne Gretzky",
+  "The best way to predict the future is to create it. – Peter Drucker"
+];
+
+// Route to get a random quote
+app.get("/quotes", (req: Request, res: Response) => {
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  const randomQuote = quotes[randomIndex];
+  return res.json({ quote: randomQuote });
+});
+
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
 
 
